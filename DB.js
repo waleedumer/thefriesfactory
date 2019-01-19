dc4e544707692758681d9feb0c0eccd367566c57
waleedumer <waleedumer42@outlook.com>
2019-01-19 14:28:10 +0500

    first commit

---
+++ DB.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB: 'mongodb+srv://waleedumer_dev:WiDiiii4@free-aws-cluster-74bpk.mongodb.net/friesFactory?retryWrites=true'
+ };
---
+++ models/Orders.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for Business
+let Orders = new Schema({
+  amount: {
+    type: String
+  },
+  order_date: {
+    type: String
+  },
+  orderId: {
+    type: Number
+  },
+  deliveryBy: {
+    type: String
+  }
+},{
+    collection: 'orders'
+});
+
+module.exports = mongoose.model('Orders', Orders);
