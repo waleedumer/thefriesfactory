ffdb76f1bc524aae18562cdb9e814cf337df926a
waleedumer <waleedumer42@outlook.com>
2019-01-22 18:08:33 +0500

    Update business.route.js

--- routes/business.route.js
+++ routes/business.route.js
@@ -25,7 +25,7 @@ businessRoutes.route('/add',{ automatic405: true }).post(function (req, res) {
 // Defined get data(index or listing) route
 businessRoutes.route('/').get(function (req, res) {
   //{order_date: req.query.orderDate},
-    Business.find(function (err, businesses){
+    Business.find().sort({order_date: -1},function (err, businesses){
     if(err){
       console.log(err);
     }
