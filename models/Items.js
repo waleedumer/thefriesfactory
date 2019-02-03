9100646112129b8ce9e624bc35506bc72a610b2b
waleedumer <waleedumer42@outlook.com>
2019-02-03 10:28:43 +0500

    Create Items.js

---
+++ models/Items.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for Business
+let Items = new Schema({
+  _id: {
+    type: String
+  },
+  itemId: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  sale_price: {
+    type: String
+  },
+  purchase_price: {
+    type: String
+  }
+},{
+    collection: 'items'
+});
+
+module.exports = mongoose.model('Items', Items);
